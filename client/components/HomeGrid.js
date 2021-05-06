4713d6c969c33d1c6d2c39d1fb2d70f8f81c2e87
Jake Chen <jakechen825@gmail.com>
2021-05-05 22:34:06 -0400

    FE: (FileUpload, home, HomeGrid, ModalButton) added modal tab to combine with FileUpload

--- client/components/HomeGrid.js
+++ client/components/HomeGrid.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import { Button, Typography, Paper, Grid } from '@material-ui/core';
+import { Typography, Paper, Grid } from '@material-ui/core';
+import FileUpload from './FileUpload';
+import ModalButton from './ModalButton';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,24 +28,12 @@ const useStyles = makeStyles((theme) => ({
 export default function HomeGrid() {
   const classes = useStyles();
 
-  const handleOnClick = () => {
-    console.log('Clicked!');
-  };
-
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
         <Grid item>
           <Typography variant="body2" style={{ cursor: 'pointer' }}>
-            <Button
-              variant="contained"
-              color="default"
-              className={classes.button}
-              startIcon={<CloudUploadIcon />}
-              onClick={handleOnClick}
-            >
-              Import Data
-            </Button>
+            <ModalButton />
           </Typography>
         </Grid>
       </Paper>
--- client/components/home.js
+++ client/components/home.js
@@ -1,28 +1,28 @@
-import React from "react";
-import { connect } from "react-redux";
-import FileUpload from "./FileUpload";
+import React from 'react';
+import { connect } from 'react-redux';
+import HomeGrid from './HomeGrid';
 
 /**
  * COMPONENT
  */
 export const Home = (props) => {
-	const { username } = props;
+  const { username } = props;
 
-	return (
-		<div>
-			<h3>Welcome, {username}</h3>
-			<FileUpload />
-		</div>
-	);
+  return (
+    <div>
+      <h3>Welcome, {username}</h3>
+      <HomeGrid />
+    </div>
+  );
 };
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-	return {
-		username: state.auth.username,
-	};
+  return {
+    username: state.auth.username,
+  };
 };
 
 export default connect(mapState)(Home);
