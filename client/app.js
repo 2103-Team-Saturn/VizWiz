d1bf5d7396065b35f4d34a33618f81181eb4ada4
Jake Chen <jakechen825@gmail.com>
2021-05-07 16:29:00 -0400

    resolve issues

--- client/app.js
+++ client/app.js
@@ -2,11 +2,17 @@ import React from "react";
 
 import Navbar from "./components/navbar";
 import Routes from "./routes";
+import LineGraph from "./components/LineGraph";
+import EditChart from "./components/EditChart";
+import PieGraph from "./components/PieGraph";
 
 const App = () => {
 	return (
 		<div>
 			<Navbar />
+			<EditChart />
+			{/* <LineGraph /> */}
+			{/* <PieGraph /> */}
 			<Routes />
 		</div>
 	);
