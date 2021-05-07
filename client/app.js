b41cc5053965fcfad7f7d3020aad2bb47fc2d5e2
isabellenoelnelson <isabellenoelnelson@gmail.com>
2021-05-07 15:37:45 -0400

    Debugging: Fixed cases

--- client/app.js
+++ client/app.js
@@ -1,21 +1,21 @@
 import React from "react";
 
-import Navbar from './components/Navbar'
-import Routes from './routes'
-import LineGraph from './components/LineGraph'
-import EditChart from './components/EditChart'
-import PieGraph from './components/PieGraph'
+import Navbar from "./components/navbar";
+import Routes from "./routes";
+import LineGraph from "./components/LineGraph";
+import EditChart from "./components/EditChart";
+import PieGraph from "./components/PieGraph";
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <EditChart />
-      {/* <LineGraph /> */}
-      {/* <PieGraph /> */}
-      <Routes />
-    </div>
-  )
-}
+	return (
+		<div>
+			<Navbar />
+			<EditChart />
+			{/* <LineGraph /> */}
+			{/* <PieGraph /> */}
+			<Routes />
+		</div>
+	);
+};
 
 export default App;
