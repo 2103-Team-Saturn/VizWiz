a2124a1c848aa42baf23cfe6a0fb490bcca7e9c3
Jake Chen <jakechen825@gmail.com>
2021-05-10 17:43:39 -0400

    (FE) added animation to bar and pie

--- client/app.js
+++ client/app.js
@@ -4,7 +4,6 @@ import Navbar from './components/navbar';
 import Routes from './routes';
 import LineGraph from './components/graphCharts/LineGraph';
 import PieGraph from './components/graphCharts/PieGraph';
-import EditChart from './components/util.js/EditChart';
 
 const App = () => {
   return (
--- client/components/graphCharts/BarGraph.js
+++ client/components/graphCharts/BarGraph.js
@@ -1,17 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 import {
-	VictoryBar,
-	VictoryChart,
-	VictoryAxis,
-	VictoryStack,
-	VictoryTheme,
-	VictoryTooltip,
-	VictoryLabel,
-} from "victory";
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryStack,
+  VictoryTheme,
+  VictoryTooltip,
+  VictoryLabel,
+} from 'victory';
 
 export default class BarGraph extends Component {
-
   render() {
     console.log('**BG props', this.props);
     const { data, dataset, x, y } = this.props;
@@ -23,75 +22,75 @@ export default class BarGraph extends Component {
       <div id="graph">
         <VictoryChart
           theme={VictoryTheme.material}
-          style={{ parent: { maxWidth: "100%" } }}
+          style={{ parent: { maxWidth: '100%' } }}
           domainPadding={45}
           width={500}
           height={350}
           padding={{ left: 100, right: 25, top: 35, bottom: 75 }}
         >
-            <VictoryLabel
-              text={dataset}
-              style={{
-                fontSize: 20,
-                textAnchor: 'start',
-                verticalAnchor: 'end',
-                fill: '#455A64',
-                fontFamily: 'inherit'
-              }}
-              x={100}
-              y={24}
-            />
-            <VictoryAxis
-              label={x}
-              fixLabelOverlap={true}
-              style={{
-                axis: {stroke: '#756f6a'},
-                axisLabel: {fontSize: 16, padding: 60},
-                tickLabels: {angle: 20}
-              }}
-              tickValues={data.map(d => d[x])}
-              tickFormat={data.map(d => {
-                if (typeof d[x] === 'string') {
-                  if (x === 'Month' || x === 'Day') {
-                    return d[x].slice(0, 3)
-                  } else {
-                    return d[x]
-                  }
+          <VictoryLabel
+            text={dataset}
+            style={{
+              fontSize: 20,
+              textAnchor: 'start',
+              verticalAnchor: 'end',
+              fill: '#455A64',
+              fontFamily: 'inherit',
+            }}
+            x={100}
+            y={24}
+          />
+          <VictoryAxis
+            label={x}
+            fixLabelOverlap={true}
+            style={{
+              axis: { stroke: '#756f6a' },
+              axisLabel: { fontSize: 16, padding: 60 },
+              tickLabels: { angle: 20 },
+            }}
+            tickValues={data.map((d) => d[x])}
+            tickFormat={data.map((d) => {
+              if (typeof d[x] === 'string') {
+                if (x === 'Month' || x === 'Day') {
+                  return d[x].slice(0, 3);
                 } else {
-                  return d[x]
+                  return d[x];
                 }
-              })}
-            />
-            <VictoryAxis
-              dependentAxis
-              fixLabelOverlap={true}
-              label={y}
-              style={{
-                axis: {stroke: '#756f6a'},
-                axisLabel: {fontSize: 16, padding: 80}
-              }}
-              tickFormat={y}
-            />
+              } else {
+                return d[x];
+              }
+            })}
+          />
+          <VictoryAxis
+            dependentAxis
+            fixLabelOverlap={true}
+            label={y}
+            style={{
+              axis: { stroke: '#756f6a' },
+              axisLabel: { fontSize: 16, padding: 80 },
+            }}
+            tickFormat={y}
+          />
+          <VictoryStack>
             <VictoryBar
-            data={data}
-            // data={data.map(d => {
-            //     // console.log('*d*>>>', d)
-            //       return d
-            //     })}
-                x={x}
-                y={y}
-                // domain={ {x: [0, x.length + 1], y: [0, (Math.max(...y) + 20) ] } }
-                barRatio={0.2}
-                style={{
-                    data: {
-                      fill: "#c43a31",
-                      width: 24
-                    }
-                  }}
-              />
+              data={data}
+              x={x}
+              y={y}
+              barRatio={0.2}
+              style={{
+                data: {
+                  fill: '#c43a31',
+                  width: 24,
+                },
+              }}
+              animate={{
+                duration: 2000,
+                onLoad: { duration: 1000 },
+              }}
+            />
+          </VictoryStack>
         </VictoryChart>
       </div>
     );
   }
-
 }
