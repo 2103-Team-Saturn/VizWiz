ec7600b7075f5b661e117823fd104a881533d2b4
isabellenoelnelson <isabellenoelnelson@gmail.com>
2021-05-10 15:15:11 -0400

    FE: Renders Line Chart and Scatter Chart

---
+++ client/components/ModalButton.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import { Button } from "@material-ui/core";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import FileUpload from "./FileUpload";
+
+const useStyles = makeStyles((theme) => ({
+	modal: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	paper: {
+		backgroundColor: theme.palette.background.paper,
+		border: "2px solid #000",
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3),
+	},
+}));
+
+export default function TransitionsModal() {
+	const classes = useStyles();
+	const [open, setOpen] = React.useState(false);
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	return (
+		<div>
+			<Button
+				variant='contained'
+				color='default'
+				className={classes.button}
+				startIcon={<CloudUploadIcon />}
+				onClick={handleOpen}>
+				Upload
+			</Button>
+			<Modal
+				aria-labelledby='transition-modal-title'
+				aria-describedby='transition-modal-description'
+				className={classes.modal}
+				open={open}
+				onClose={handleClose}
+				closeAfterTransition
+				BackdropComponent={Backdrop}
+				BackdropProps={{
+					timeout: 500,
+				}}>
+				<Fade in={open}>
+					<div className={classes.paper}>
+						<FileUpload />
+					</div>
+				</Fade>
+			</Modal>
+		</div>
+	);
+}
