2e65efe09ab5c84335ed23f6c866f19f4ace1598
isabellenoelnelson <isabellenoelnelson@gmail.com>
2021-05-19 16:38:33 -0400

    FE: Fixes bug where user couldnt edit chart if they hadnt opened a room

--- client/components/historyScreen/ChartHistory.js
+++ client/components/historyScreen/ChartHistory.js
@@ -4,70 +4,71 @@ import { Link } from "react-router-dom";
 import { gotGraphs, deletingGraph } from "../../store/graph";
 import { Grid, Paper, Container, Button } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
+import { fetchAllUsers } from "../../store/users";
 
 class ChartHistory extends Component {
-  constructor() {
-    super();
-    this.deleteGraph = this.deleteGraph.bind(this);
-  }
+	constructor() {
+		super();
+		this.deleteGraph = this.deleteGraph.bind(this);
+	}
 
-  componentDidMount() {
-    this.props.gotGraphs(this.props.userId);
-  }
+	componentDidMount() {
+		this.props.fetchAllUsers();
+		this.props.gotGraphs(this.props.userId);
+	}
 
-  deleteGraph(graph) {
-    this.props.deletingGraph(graph);
-  }
+	deleteGraph(graph) {
+		this.props.deletingGraph(graph);
+	}
 
-  render() {
-    console.log("props", this.props);
-    return (
-      <div>
-        <Container>
-          <h1 id="HistoryTitle">Graph Dashboard</h1>
-          <Grid container spacing={10}>
-            {this.props.userGraph.map((graph) => (
-              <Grid key={graph.id} item xs={12} s={6} md={4} lg={3}>
-                <Link
-                  to={{
-                    pathname: `/users/${graph.userId}/data/${graph.datumId}/${graph.id}`,
-                    state: {
-                      graph,
-                    },
-                  }}
-                >
-                  <h1>{graph.properties.title}</h1>
-                </Link>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="secondary"
-                  onClick={() => this.deleteGraph(graph)}
-                >
-                  Delete <DeleteIcon />
-                </Button>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </div>
-    );
-  }
+	render() {
+		console.log("props", this.props);
+		return (
+			<div>
+				<Container>
+					<h1 id='HistoryTitle'>Graph Dashboard</h1>
+					<Grid container spacing={10}>
+						{this.props.userGraph.map((graph) => (
+							<Grid key={graph.id} item xs={12} s={6} md={4} lg={3}>
+								<Link
+									to={{
+										pathname: `/users/${graph.userId}/data/${graph.datumId}/${graph.id}`,
+										state: {
+											graph,
+										},
+									}}>
+									<h1>{graph.properties.title}</h1>
+								</Link>
+								<Button
+									type='submit'
+									fullWidth
+									variant='contained'
+									color='secondary'
+									onClick={() => this.deleteGraph(graph)}>
+									Delete <DeleteIcon />
+								</Button>
+							</Grid>
+						))}
+					</Grid>
+				</Container>
+			</div>
+		);
+	}
 }
 
 const mapState = (state) => {
-  return {
-    userId: state.auth.id,
-    userGraph: state.graphs,
-  };
+	return {
+		userId: state.auth.id,
+		userGraph: state.graphs,
+	};
 };
 
 const mapDispatch = (dispatch) => {
-  return {
-    gotGraphs: (id) => dispatch(gotGraphs(id)),
-    deletingGraph: (id) => dispatch(deletingGraph(id)),
-  };
+	return {
+		gotGraphs: (id) => dispatch(gotGraphs(id)),
+		deletingGraph: (id) => dispatch(deletingGraph(id)),
+		fetchAllUsers: () => dispatch(fetchAllUsers()),
+	};
 };
 
 export default connect(mapState, mapDispatch)(ChartHistory);
