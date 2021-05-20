742d64a43f1e886bd20fafe898bc444e12b5e61e
Jeanettable <jeanetteabell@gmail.com>
2021-05-20 11:58:27 -0700

    pre-main merge progress

--- client/components/LeftDrawer.js
+++ client/components/LeftDrawer.js
@@ -76,7 +76,7 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     alignItems: "center",
     justifyContent: "flex-end",
-    padding: theme.spacing(0, 1),
+    padding: theme.spacing(12, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
   },
@@ -93,6 +93,7 @@ const useStyles = makeStyles((theme) => ({
   },
   logout: {
     // AHHHHHHH!!!!
+    alignSelf: "flex-end",
   },
 }));
 
